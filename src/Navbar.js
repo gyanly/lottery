e143fb71f5911a2c3e3368062797010d6a4b16e0
gyanly <gyancv@gmail.com>
2022-11-02 14:46:10 +0530

    fsdlfasljk

---
+++ src/Navbar.js
@@ -0,0 +1,14 @@
+function Navbar() {  
+  
+    return (
+      <div className="navbar">
+        <h1>Blog</h1>    
+        <div className="link">
+            <a href="/">Home</a>
+            <a href="/create">New Blog</a>           
+        </div>     
+      </div>   
+    );
+  }
+  
+  export default Navbar;
